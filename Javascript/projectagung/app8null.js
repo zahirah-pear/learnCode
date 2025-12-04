f2ffb6235eb34f8da8c96d75eb6c04145095c305
zahirah-pear <malikanashitazahirah@gmail.com>
2025-12-04 20:55:52 +0700

    projectagung

---
+++ Javascript/projectagung/app8null.js
@@ -0,0 +1,10 @@
+// undefined    typeof=undifined
+// null         typeof=object
+
+const shoeMerk = adidas;
+const bycicleMerk = null;
+const motorMerk = honda;
+const carMerk = porche;
+const shipMerk = null;
+const airplaneMerk = airbus;
+
