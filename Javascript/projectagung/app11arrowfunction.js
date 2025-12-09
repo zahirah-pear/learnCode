d4275deed7b812278a41abc8c1117cc4eef7aa4b
zahirah-pear <malikanashitazahirah@gmail.com>
2025-12-09 23:51:52 +0700

    projectagung

---
+++ Javascript/projectagung/app11arrowfunction.js
@@ -0,0 +1,18 @@
+const sayHi = (name) => {
+
+    console.log('Hi ' + name);
+};
+
+function sayHi2 (name){
+    console.log('Hi ' + name );
+};
+
+// ksempulannya arrow function pakek arrow dan const
+
+const sayBye = (name) => {
+    console.log("Bye, " + name);
+};
+
+const add = (x, y) => {
+    return x + y;
+};
