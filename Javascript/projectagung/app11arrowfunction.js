f2969acd47501ccf1b62c1803347566f06f4c4a5
zahirah-pear <malikanashitazahirah@gmail.com>
2025-12-10 00:05:04 +0700

    projectagung

--- Javascript/projectagung/app11arrowfunction.js
+++ Javascript/projectagung/app11arrowfunction.js
@@ -13,6 +13,13 @@ const sayBye = (name) => {
     console.log("Bye, " + name);
 };
 
-const add = (x, y) => {
-    return x + y;
-};
+const add2 = (x, y) => x + y;
+
+// versi panjang 
+// const add = (x, y) => {
+//    return x + y;
+// };
+
+const identity = (name, age) => name + " adalah umur " + age;
+
+console.log(identity("Malika", 17));
