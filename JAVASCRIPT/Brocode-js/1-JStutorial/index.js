b0e7c0bd83798e7d9a179ced7c048c32652f87c7
zahirah-pear <malikanashitazahirah@gmail.com>
2026-01-03 22:20:11 +0700

    new

---
+++ JAVASCRIPT/Brocode-js/1-JStutorial/index.js
@@ -0,0 +1,11 @@
+//console.log(3);
+//console.log(`I like mie ayam`);
+
+//window.alert(`Furry the worst`);
+/*
+    window.alert    = untuk notif di awal buka web
+    console.log     = untuk print sesuatu bisa kalimat, huruf, simbol (gunakan titik miring di atas (``)), angka
+*/
+document.getElementById(`FisrtH1`);
+//or
+document.getElementById(`FirstP`).textContent = `Hellour` ;
